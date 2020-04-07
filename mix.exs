a188e1f8dd0e2764a891e84174e7a60226f67805
Matt Roman <romanmt@gmail.com>
2020-04-07 10:17:41 -0400

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Textex.Mixfile do
   def project do
     [
       app:             :textex,
-      version:         "0.2.2",
+      version:         "0.3.0",
       elixir:          "~> 1.2",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
